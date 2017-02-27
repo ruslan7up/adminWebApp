368e3f373b4e9bf223b7e79b6069ff5ac2834922
ruslan7up <ruslan7up@gmail.com>
2017-02-27 16:21:44 +0600

    fixes

--- src/main/webapp/WEB-INF/resources/js/newsAddScripts.js
+++ src/main/webapp/WEB-INF/resources/js/newsAddScripts.js
@@ -2,12 +2,10 @@
  * Created by ruslan on 07.02.2017.
  */
 $('#btnsubmit').on('click',function () {
-    alert($('#files')[0].files.length);
     var data = new FormData();
     data.append("title",$('#title').val());
     data.append("text", $('#newstext').val());
     data.enctype = "multipart/form-data; charset=utf-8";
-    alert(document.inputEncoding);
     for(var i = 0; i < $('#files')[0].files.length; i++) {
         data.append("file",$('#files')[0].files[i]);
     }
